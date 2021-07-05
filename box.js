6e537aaeb2ef6dd372b79e80ffc1c292982883dc
priyanka-sidhuraj <71912936+priyanka-sidhuraj@users.noreply.github.com>
2021-07-05 17:50:54 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,27 @@
+class Simrit{
+
+constructor(){
+
+this.x=200
+this.y=250
+this.h=20
+this.w=10
+
+
+}
+ show(){
+
+rect(this.x,this.y,this.w,this.h)
+
+
+}
+
+changewidth(x){
+
+  this.w=x
+}
+
+}
+  
+
+  
---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function setup() 
+{
+  createCanvas(400, 400);
+   
+  box = new Simrit()
+
+
+}
+
+function draw() 
+{
+  background(220);
+  box.show()
+  box.changewidth(50)
+}
+
